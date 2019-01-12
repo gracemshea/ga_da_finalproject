666678443ab93165e578ad0eb6405bb12d0c4c8a
gracemshea <33272959+gracemshea@users.noreply.github.com>
2019-01-12 09:41:06 -0800

    Create temptable.sql

---
+++ sqlfiles/load/temptable.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+CREATE SCHEMA IF NOT EXISTS outdoors;
+
+CREATE TEMPORARY TABLE outdoors.population(
+
+  citytown varchar(30),
+  stname varchar(30),
+  popestimate2017 integer (10),
+  );
+
+COPY outdoors.population FROM '/Users/graceshea/Documents/Galvanize/ga_da_finalproject/data/population.csv'
+WITH HEADER DELIMITER ',' CSV;
+COMMIT;
